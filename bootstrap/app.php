d45636b224abd14c414ea65dd7925a742d841907
haginmc <Haginmcu@gmail.com>
2026-09-01 15:52:44 +0800

    Fix trustproxy round 2

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__ . '/../routes/console.php',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
         $middleware->alias([
             'is_admin' => \App\Http\Middleware\IsAdmin::class,
             'is_super_admin' => \App\Http\Middleware\IsSuperAdmin::class,
